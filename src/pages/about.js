666b5d1918892f4415fa9a65c40cea93560b981a
James Seconde <jim.seconde@googlemail.com>
2019-07-02 08:15:26 +0100

    Basic about info
    placeholder

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import H2 from '../components/H2';
+import H2 from "../components/H2";
+import P from "../components/P";
 
-import Layout from '../templates/layout';
+import Layout from "../templates/layout";
 
 const Wrapper = styled.div`
   text-align: center;
 `;
 
-function NotFound() {
+function About() {
   return (
     <Layout>
-    <Wrapper>
-      <H2>About Page you mothers</H2>
-    </Wrapper>
+      <Wrapper>
+        <H2>Hi, I'm James!</H2>
+        <P>
+          My whole life has been straddling the worlds of theatre, writing and
+          software development. As an emerging Developer Advocate, I see an
+          emerging new world of software development that needs shouting about,
+          that needs shaping to find it's way, that needs to fit into a digital
+          puzzle of diciplines.
+        </P>
+        <P>
+          I'm a graduate in English Literature and Theatre, and have been an
+          actor, a Technical Manager at a London theatre, A Data Analyst with
+          Microsoft & Oracle SQL tooling and a Web Developer. At one time I
+          pretended to be a DJ.
+        </P>
+        <P>
+          I run Birmingham's PHP usergroup, brumPHP, and co-run a quarterly tech
+          conference, Fusion Birmingham. I write microservices for Shopify in
+          Laravel and Vue.js for EastsideCo in Digbeth, Birmingham.
+        </P>
+      </Wrapper>
     </Layout>
   );
 }
 
-export default NotFound;
+export default About;
