6ad280fe95c873a65faa4455e17f50af41d790d8
James Seconde <jim.seconde@googlemail.com>
2019-07-09 08:25:15 +0100

    maybe not, undoing

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,29 +10,22 @@ const Wrapper = styled.div`
   text-align: center;
 `;
 
-const TextBody = styled.div`
-  text-align: left;
-  font-size: 14px;
-`;
-
 function About() {
   return (
     <Layout>
       <Wrapper>
         <Container>
           <H2>Hi, I'm James!</H2>
-          <TextBody>
-            <p>
+            <P>
               From Theatre Graduate, to QA, to SQL Data Analytics, to Web Developer, to Developer Advocate.
               From the stage and back to the stage, I sit in a happy medium between tech and theatre. I also
               pretended to be a <a href="https://drive.google.com/open?id=0B2almTqbTKrIeDBMUjNMRkNITDQ">DJ</a> on the way.
-            </p>
-            <p>
+            </P>
+            <P>
               I run Birmingham's PHP usergroup, brumPHP, and co-run a quarterly tech
               conference, Fusion Birmingham. I write microservices for Shopify in
               Laravel and Vue.js for Eastside Co in Digbeth, Birmingham. I speak on tech and culture.
-            </p>
-          </TextBody>
+            </P>
         </Container>
       </Wrapper>
     </Layout>
