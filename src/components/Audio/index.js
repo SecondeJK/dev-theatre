e06b755668aa4da35fa5da2583dde3eca04434f3
Jim Seconde <jim.seconde@googlemail.com>
2020-03-10 12:44:00 +0000

    dev radio launch

---
+++ src/components/Audio/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const AUDIO = styled.audio`
+  width: 100%;
+  display: inline-block;
+  padding: 5px 15px;
+
+  &:hover {
+    border-color: #000;
+  }
+`;
+
+export default AUDIO;
--- src/pages/devradio.js
+++ src/pages/devradio.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../templates/layout";
 import Container from '../components/Container';
 import H1 from '../components/H1';
+import AUDIO from "../components/Audio";
 import P from "../components/P";
 
 function DevRadio() {
@@ -9,8 +10,20 @@ function DevRadio() {
         <Layout>
             <Container>
                 <H1>The Dev Radio Podcast</H1>
-                <P>The Dev Radio Podcast is currently in development, and will contain 1hr 30min sessions of my rather
-                eclectic musical tastes.</P>
+                <P> Welcome to The Dev Radio! A music show straight from the basement of your local record shop.</P>
+                <br/>
+                <div>
+                    <P>001: Welcome!</P>
+                    <AUDIO
+                        controls
+                        src="https://devtheatre.s3-eu-west-1.amazonaws.com/The+Dev+Radio+001.mp3">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                    </AUDIO>
+                    <form method="get" action="https://devtheatre.s3-eu-west-1.amazonaws.com/The+Dev+Radio+001.mp3">
+                        <input type="submit" value="Download" />
+                    </form>
+                </div>
             </Container>
         </Layout>
     );
