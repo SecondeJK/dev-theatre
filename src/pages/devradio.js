1960116a558c47f28dfac098a3ce34d3533e404e
James Seconde <jim.seconde@googlemail.com>
2020-04-06 09:02:34 +0000

    006

--- src/pages/devradio.js
+++ src/pages/devradio.js
@@ -59,6 +59,15 @@ function DevRadio() {
                         <code>audio</code> element.
                     </AUDIO>
                 </div>
+                <div>
+                    <P>006</P>
+                    <AUDIO
+                        controls
+                        src="https://devtheatre.s3-eu-west-1.amazonaws.com/The+Dev+Radio+006.mp3">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                    </AUDIO>
+                </div>
             </Container>
         </Layout>
     );
