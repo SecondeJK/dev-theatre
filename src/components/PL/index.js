62582d5e9c5d472701422e43261d83c50c1339c9
Jim Seconde <jim.seconde@googlemail.com>
2020-07-01 22:37:27 +0100

    Dev Radio 1 is now live

---
+++ src/components/PL/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const PL = styled.p`
+  color: #666d71;
+  display: block;
+  font-size: 1.6em;
+  margin: 0 0 0 15px;
+  text-align: left;
+`;
+
+export default PL;
--- src/pages/devtheatrepodcast.js
+++ src/pages/devtheatrepodcast.js
@@ -4,6 +4,9 @@ import Container from '../components/Container';
 import H1 from '../components/H1';
 import P from "../components/P";
 import setup from "../../static/devtheatre-setup.jpg";
+import AUDIO from "../components/Audio";
+import H2 from "../components/H2";
+import PL from "../components/PL";
 
 function DevTheatrePodcast() {
     return (
@@ -12,8 +15,20 @@ function DevTheatrePodcast() {
                 <H1>The Dev Theatre Podcast</H1>
                 <br/>
                 <img src={setup}></img>
-                <P>DevOps, Developer Relations, Tech Culture,
-                    Nonsense - join me in episodes of the Dev Theatre Podcast, where I'll speak to my peers.</P>
+                <P>DevOps, Developer Relations, Tech Culture, Engineering,
+                    Nonsense - join me in episodes of the Dev Theatre Podcast, where I'll speak to my peers around me on tech stuff.</P>
+                <br/>
+                <div>
+                    <H2>Episode 1: On Javascript & The Future with Stuart Langridge</H2>
+                    <PL>Expanding on his talk "You Really Don't Need All That Javascript",
+                        listen to chat about npm, dependency management, Deno, and standard libraries.</PL>
+                    <AUDIO
+                        controls
+                        src="https://devtheatre.s3-eu-west-1.amazonaws.com/The+Dev+Theatre+1.mp3">
+                        Your browser does not support the
+                        <code>audio</code> element.
+                    </AUDIO>>
+                </div>
             </Container>
         </Layout>
     );
